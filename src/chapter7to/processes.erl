ca62a5426574c886096cef417f75466ef7050b3c
seet61 <seet_seet@mail.ru>
2019-01-08 23:08:06 +0300

    8.4

---
+++ src/chapter7to/processes.erl
@@ -0,0 +1,38 @@
+%%%-------------------------------------------------------------------
+%%% @author seet
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 08. Янв. 2019 22:43
+%%%-------------------------------------------------------------------
+-module(processes).
+-author("seet").
+
+%% API
+-export([max/1]).
+
+%% max(N)
+%% Create N processes then destroy them
+%% See how much time this takes
+
+max(N) ->
+  Max = erlang:system_info(process_limit),
+  io:format("*Maximum allowes processes: ~p~n*", [Max]),
+  statistics(runtime),
+  statistics(wall_clock),
+  L = for(1, N, fun() -> spawn(fun() -> wait() end) end),
+  {_, Time1} = statistics(runtime),
+  {_, Time2} = statistics(wall_clock),
+  lists:foreach(fun(Pid) -> Pid ! die end, L),
+  U1 = Time1 * 1000 / N,
+  U2 = Time2 * 1000 / N,
+  io:format("*Process spawn time=~p (~p) microseconds~n*", [U1, U2]).
+
+wait() ->
+  receive
+    die -> void
+  end.
+
+for(N, N, F) -> [F()];
+for(I, N, F) -> [F() | for(I+1, N, F)].
