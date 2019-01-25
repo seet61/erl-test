ab057e3a63f5aa4acd30e918587c0f4ea5ce0f72
seet61 <seet_seet@mail.ru>
2019-01-25 11:35:03 +0300

    10.1

---
+++ src/chapter10to/kvs.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------
+%%% @author seet
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. Янв. 2019 11:21
+%%%-------------------------------------------------------------------
+-module(kvs).
+-author("seet").
+
+%% API
+-export([start/0, store/2, lookup/1]).
+
+start() -> register(kvs, spawn(fun() -> loop() end)).
+
+store(Key, Value) -> rpc({store, Key, Value}).
+
+lookup(Key) -> rpc({lookup, Key}).
+
+rpc(Q) ->
+  kvs ! {self(), Q},
+  receive
+    {kvs, Reply} ->
+      Reply
+  end.
+
+loop() ->
+  receive
+    {From, {store, Key, Value}} ->
+      put(Key, {ok, Value}),
+      From ! {kvs, true},
+      loop();
+    {From, {lookup, Key}} ->
+      From ! {kvs, get(Key)},
+      loop()
+  end.
